97b1ec9e8139f8fe20a303e1c340d40af70414df
Loic PENNEQUIN <lpennequin@a-sis.division-savoye.ad>
2018-08-07 17:57:13 +0200

    user online indicator

--- src/services/socketIo.js
+++ src/services/socketIo.js
@@ -10,37 +10,46 @@ module.exports.init = (server) =>{
     let sockets = [];
 
     io.on('connection', socket => {
-        socket.on('user logged in', async ({id}) => {
-            if (!socket.user) {
-                socket.user = (await models.User.where('id', id).fetch({withRelated: 'contacts'})).toJSON();
-                socket.user.socketId = socket.id;
-                console.log(`${socket.user.username} logged in`);
-
+        const emitToFriends = (message, data) => {
+            if (socket.user) {
                 for (let socketId in io.sockets.sockets) {
                     let user = io.sockets.sockets[socketId].user;
                     if (user){
                         socket.user.contacts.forEach(contact => {
                             if ( contact.contact_id === user.id ){
-                                io.to(`${user.socketId}`).emit('contact logged in', { id : socket.user.id })
+                                console.log('should emit ' + message + ' to '+ user.username );
+                                io.to(`${user.socketId}`).emit(message, data)
                             }
                         })
                     }
                 }
             }
+        }
+
+        socket.on('user logged in', async ({id}) => {
+            socket.user = (await models.User.where('id', id).fetch({withRelated: 'contacts'})).toJSON();
+            socket.user.socketId = socket.id;
+
+            emitToFriends('contact logged in', { id : socket.user.id });
+            let onlineFriends = [];
+
+            for (let socketId in io.sockets.sockets) {
+                let user = io.sockets.sockets[socketId].user;
+                if (user) {
+                    socket.user.contacts.some(c => {
+                        c.id === user.id}
+                    )
+                    if ( socket.user.contacts.some(c => c.contact_id === user.id)) {
+                        onlineFriends.push(user.id);
+                    }
+                }
+            }
+            socket.emit('get online contacts', onlineFriends);
         });
 
         socket.on('disconnect',  () => {
-            if ( socket.user ){
-                for (let socketId in io.sockets.sockets) {
-                    let user = io.sockets.sockets[socketId].user;
-                    if (user){
-                        socket.user.contacts.forEach(contact => {
-                            if ( contact.contact_id === user.id ){
-                                io.to(`${user.socketId}`).emit('contact logged out', { id : socket.user.id })
-                            }
-                        })
-                    }
-                }
+            if (socket.user){
+                emitToFriends('contact logged out', { id : socket.user.id });
             }
         })
     });
